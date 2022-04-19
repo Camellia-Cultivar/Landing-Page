993964a9ba39985062c4f3ba6a3da61f56a92ffc
Fábio Martins <fabio.m@ua.pt>
2022-04-19 21:03:56 +0100

    added spacing and new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    
+    extend: {
+      colors:{
+        'primary': '#00dc82',
+        'faded-primary':"#DFFBEF",
+        'dark-primary':"#003720",
+      },
+      spacing:{
+        '25':'6.25rem',
+        '35':'9.375rem',
+        '50':'12.5rem',
+      },
+    },
   },
   variants: {
     extend: {},
