7fbd32648b7f661eaff29eba265dee05f4e928aa
Fábio Martins <fabio.m@ua.pt>
2022-04-25 17:20:57 +0100

    moved styles to assets/css

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,6 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     '@/assets/css/main.css',
-    '~/styles/index.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
